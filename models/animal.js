7fc138ff2cbf6aa237c1dc5200a11a6999bd3bfb
Sakinah Freeman <freemansakinah@gmail.com>
2022-11-29 22:20:32 -0500

    creates seed route

--- models/animal.js
+++ models/animal.js
@@ -9,6 +9,7 @@ const mongoose = require("./connection")
 const { Schema, model } = mongoose
 
 const animalsSchema = new Schema({
+    name: String,
     species: String,
     extinct: Boolean,
     location: String,
